217ab599af6051d441491b396bb294bc354da89a
yuva05082006 <nvyuva05@gmail.com>
2026-01-01 20:13:36 +0530

    Add files via upload
    They are basic building blocks for data manipulation and directly store simple values in memory

---
+++ Allprimitivedatatypes.java.java
@@ -0,0 +1,21 @@
+class AllPrimitiveTypes {
+    static byte a = 10;
+    static short b = 200;
+    static int c = 5000;
+    static long d = 123456789L;
+    static  float e = 12.5f;
+    static  double f = 99.99;
+    static  char g = 'A';
+     static boolean h = true;
+
+    public static void main(String[] args) {
+        System.out.println("Byte    : " + a);
+        System.out.println("Short   : " + b);
+        System.out.println("Int     : " + c);
+        System.out.println("Long    : " + d);
+        System.out.println("Float   : " + e);
+        System.out.println("Double  : " + f);
+        System.out.println("Char    : " + g);
+        System.out.println("Boolean : " + h);
+    }
+}
